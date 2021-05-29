dcf10485f7374e0dc781f4024ff7ca3057c7baf9
ptrickd <patrickdion8@gmail.com>
2021-05-29 09:39:27 -0600

    connected to mongo

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,15 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            MONGO_URI: string;
+            NODE_ENV: 'development' | 'production';
+            PORT?: string;
+            PWD: string;
+        }
+    }
+}
+
+export { }
--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,3 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 
+export default async (req: NextApiRequest, res: NextApiResponse) => {
 
+    res.json({ test: 'test' })
+
+}
