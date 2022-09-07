c2094d9d67e30c9b9317198ff6e1f5f1d3a557b9
ptrickd <patrickdion8@gmail.com>
2022-09-07 16:03:56 -0600

    Set up Register page testing

---
+++ __test__/register.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import Register from '../pages/register'
+
+import { REGISTER } from '../graphql/queries'
+
+const mocks: any = [{
+    request: {
+        query: REGISTER,
+        variables: {
+            email: 'rick@email.com',
+            password: '987654'
+        }
+    },
+    result: {
+        data: {
+            login: {
+                token: '1111',
+                user: {
+                    id: '01',
+                    email: 'myemail@email.com'
+                },
+                error: null
+            }
+        }
+    }
+}]
+
+describe('<Register />', () => {
+    it('render as intended', async () => {
+
+        render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Register />
+            </MockedProvider>
+
+        )
+
+        //need  an heading
+        const loginHeading = screen.getByRole('heading', { name: 'Register' })
+        expect(loginHeading).toBeVisible()
+
+        //need an email input
+        const emailInput = screen.getByText('Email')
+        expect(emailInput).toBeVisible()
+
+        //need an password input
+        const passwordInput = screen.getByText('Password')
+        expect(passwordInput).toBeVisible()
+
+        //need a register button
+        const registerButton = screen.getByRole('button', { name: 'Register' })
+        expect(registerButton).toBeVisible()
+    })
+})
