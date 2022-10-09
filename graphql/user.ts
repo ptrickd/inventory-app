f461e83efca950376f1ab40822c0f783d38a8731
ptrickd <patrickdion8@gmail.com>
2022-10-08 18:05:05 -0600

    fix: apollo-server dependencies not valid

--- graphql/user.ts
+++ graphql/user.ts
@@ -1,22 +1,20 @@
 //GraphQl
-import { gql } from 'apollo-server'
+import { gql } from "apollo-server-micro";
 
-//Models 
-import dbConnect from '../utils/dbConnect'
-import User from '../models/user.model'
+//Models
+import dbConnect from "../utils/dbConnect";
+import User from "../models/user.model";
 
 //Auth
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-dbConnect()
+dbConnect();
 
 interface IRegister {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
-
-
 
 export const typeDef = `
     type User {
@@ -45,64 +43,57 @@ export const typeDef = `
         login(email: String!, password: String!): LoginResponse
 
     }
-`
+`;
 
 export const resolvers = {
-    Query: {
-        currentUser: async (_: any, _1: any, { user }: any) => {
-            try {
-                if (!user) return null
-                return User.findOne({ _id: user.id })
-            }
-            catch (err) {
-                console.log(err)
-                return err
-            }
-
-        },
+  Query: {
+    currentUser: async (_: any, _1: any, { user }: any) => {
+      try {
+        if (!user) return null;
+        return User.findOne({ _id: user.id });
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
     },
-    Mutation: {
-        register: async (_: any, { email, password }: IRegister) => {
+  },
+  Mutation: {
+    register: async (_: any, { email, password }: IRegister) => {
+      try {
+        let user = await User.findOne({ email });
+        if (user) throw new Error("Failed to create user");
 
-            try {
-                let user = await User.findOne({ email })
-                if (user) throw new Error('Failed to create user')
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user = new User({
+          email,
+          password: hashedPassword,
+        });
+        user = await user.save();
+        if (!user) throw new Error("Failed to create user");
+        return { user };
+      } catch (err: any) {
+        return { error: err.message };
+      }
+    },
+    login: async (_: any, { email, password }: IRegister) => {
+      try {
+        const user = await User.findOne({ email });
 
-                const hashedPassword = await bcrypt.hash(password, 10)
-                user = new User({
-                    email, password: hashedPassword
-                })
-                user = await user.save()
-                if (!user) throw new Error("Failed to create user")
-                return { user }
-            }
-            catch (err: any) {
-                return { error: err.message }
-            }
+        if (!user) throw new Error("Invalid Login");
 
-        },
-        login: async (_: any, { email, password }: IRegister) => {
-            try {
-                const user = await User.findOne({ email })
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) throw new Error("Invalid Login");
 
-                if (!user) throw new Error("Invalid Login")
+        const token = jwt.sign(
+          { id: user.id, email: user.email },
+          process.env.RESTO_JWT_SECRET,
+          { expiresIn: "30d" }
+        );
 
-                const passwordMatch = await bcrypt.compare(password, user.password)
-                if (!passwordMatch) throw new Error("Invalid Login")
-
-                const token = jwt.sign(
-                    { id: user.id, email: user.email },
-                    process.env.RESTO_JWT_SECRET,
-                    { expiresIn: '30d' }
-                )
-
-                return { token, user }
-            }
-            catch (err) {
-                return { error: 'Invalid Login' }
-            }
-
-        },
-    }
-
-}
+        return { token, user };
+      } catch (err) {
+        return { error: "Invalid Login" };
+      }
+    },
+  },
+};
