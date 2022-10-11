4f2e4bd0934b03f993dd4841595eb4ff407fad89
ptrickd <patrickdion8@gmail.com>
2022-10-11 16:28:21 -0600

    Read  environment variable to connect at the db

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,86 +1,88 @@
 //React
-import React, { useState, useEffect, useContext } from 'react'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
 
 //Material UI
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
 //Form
-import { useForm, Controller, SubmitHandler } from 'react-hook-form'
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 
 //GraphQL
-import { useMutation } from '@apollo/client'
-import { REGISTER } from '../graphql/queries'
+import { useMutation } from "@apollo/client";
+import { REGISTER } from "../graphql/queries";
 
 //Context
-import { UserContext } from '../contexts/UserContext'
+import { UserContext } from "../contexts/UserContext";
 
 //Components
-import AuthForm from '../components/AuthForm'
+import AuthForm from "../components/AuthForm";
 
 interface IForm {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     root: {
-        display: 'flex',
-        minHeight: '70vh',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignContent: 'center'
-    }
-}))
+      display: "flex",
+      minHeight: "70vh",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignContent: "center",
+    },
+  })
+);
 
 const Register: React.FC = () => {
-    const classes = useStyles()
-    const router = useRouter()
-    const { currentUser, setCurrentUser, loggedIn, setLoggedIn } = useContext(UserContext)
-    const [submitting, setSubmitting] = useState(false)
-    const [errorServerMess, setErrorServerMess] = useState('')
+  const classes = useStyles();
+  const router = useRouter();
+  const { currentUser, setCurrentUser, loggedIn, setLoggedIn } =
+    useContext(UserContext);
+  const [submitting, setSubmitting] = useState(false);
+  const [errorServerMess, setErrorServerMess] = useState("");
 
-    const { reset } = useForm<IForm>()
+  const { reset } = useForm<IForm>();
 
-    const [register] = useMutation(REGISTER)
+  const [register] = useMutation(REGISTER);
 
-    useEffect(() => {
-        if (loggedIn) {
-            router.push('/dashboard')
-        }
-    }, [loggedIn])
-
-    const onSubmit: SubmitHandler<IForm> = async (data) => {
-        setSubmitting(true)
-        const user = await register({ variables: { email: data.email, password: data.password } })
-        if (user.data.register.error) {
-            setErrorServerMess(user.data.register.error)
-        } else if (user?.data?.register.user) {
-            setErrorServerMess('')
-            router.push('/login')
-        }
-        setSubmitting(false)
-        reset({ email: '', password: '' })
-
+  useEffect(() => {
+    if (loggedIn) {
+      router.push("/dashboard");
     }
+  }, [loggedIn]);
 
-    return (
-        <Container className={classes.root} maxWidth="xs">
-            <Typography variant="h2" align="center">
-                Register
-            </Typography>
-            {errorServerMess.length > 1 && <Typography
-                align="center"
-                color="secondary"
-            >{errorServerMess}</Typography>}
-            <AuthForm onSubmit={onSubmit} submitting={submitting} label="Register" />
+  const onSubmit: SubmitHandler<IForm> = async (data) => {
+    setSubmitting(true);
+    const user = await register({
+      variables: { email: data.email, password: data.password },
+    });
+    if (user.data.register.error) {
+      setErrorServerMess(user.data.register.error);
+    } else if (user?.data?.register.user) {
+      setErrorServerMess("");
+      router.push("/login");
+    }
+    setSubmitting(false);
+    reset({ email: "", password: "" });
+  };
 
-        </Container>
+  return (
+    <Container className={classes.root} maxWidth="xs">
+      <Typography variant="h2" align="center">
+        Register
+      </Typography>
+      {errorServerMess.length > 1 && (
+        <Typography align="center" color="secondary">
+          {errorServerMess}
+        </Typography>
+      )}
+      <AuthForm onSubmit={onSubmit} submitting={submitting} label="Register" />
+    </Container>
+  );
+};
 
-    )
-}
-
-export default Register
+export default Register;
--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,32 +1,37 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 interface IConnection {
-    isConnected: number
+  isConnected: number;
 }
 
 const connection: IConnection = {
-    isConnected: 0
-}
+  isConnected: 0,
+};
 
 interface IProcessEnv {
-    [key: string]: string | undefined;
+  [key: string]: string | undefined;
 }
-
-
 
 async function dbConnect() {
-    if (connection.isConnected) {
-        return
-    }
-
-    const db = await mongoose.connect(process.env.RESTO_MONGO_URI || '', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-
-    connection.isConnected = db.connections[0].readyState
+  if (connection.isConnected) {
+    return;
+  }
+  if (process.env.NODE_ENV === "development") {
+    const db = await mongoose.connect(process.env.RESTO_MONGO_URI || "", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    connection.isConnected = db.connections[0].readyState;
+  } else if (process.env.NODE_ENV === "production") {
+    const db = await mongoose.connect(process.env.RESTO_MONGO_URI_PROD || "", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+  }
 }
 
-export default dbConnect
+export default dbConnect;
