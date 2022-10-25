d947b0a9d7426955fd2ba68e39ad0ff68714069a
ptrickd <patrickdion8@gmail.com>
2022-10-25 07:43:45 -0600

    new page loading handling state redirection pages

---
+++ pages/loading.tsx
@@ -0,0 +1,6 @@
+//page handling the loading state and managing the navigation in some cases
+//React
+import React from "react";
+export default function Loading() {
+  return <h1>Loading page</h1>;
+}
