088dfe14c5c57b9b2f319e331f9dd5a46527028a
ptrickd <patrickdion8@gmail.com>
2022-11-08 15:09:54 -0700

    Fix: Landing page appears when StatesContext

--- contexts/StatesContext.tsx
+++ contexts/StatesContext.tsx
@@ -4,11 +4,10 @@
 //when categories and products
 
 //React
-import React, { createContext, useContext, useEffect } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 //GraphQL
 import { gql, useLazyQuery, useQuery } from "@apollo/client";
-import { UserContext } from "./UserContext";
 
 //Queries
 const GET_STATES = gql`
@@ -19,56 +18,69 @@ const GET_STATES = gql`
   }
 `;
 
-interface IStates {
-  state: string;
-  report: number;
-  category: number;
-  product: number;
+interface IProps {
+  children: React.ReactNode;
 }
 
+// interface IStates {
+//   state: string;
+//   report: number;
+//   category: number;
+//   product: number;
+// }
+
 interface IContext {
-  states: IStates;
+  states: string;
 }
 
 const StatesContext = createContext<Partial<IContext>>({});
 
-const StatesProvider = () => {
-  // const { currentUser } = useContext(UserContext);
-  const { data, error, loading } = useQuery(GET_STATES);
+const StatesProvider = ({ children }: IProps) => {
+  // const [states, setStates] = useState<IStates>({
+  //   state: "loading",
+  //   report: 0,
+  //   category: 0,
+  //   product: 0,
+  // });
+  const states = "loading";
+  useEffect(() => {
+    console.log(states);
+  }, []);
+  // const { data, error, loading } = useQuery(GET_STATES);
   //states: loading, noReport, noCategory,
   // noProduct, normal, error
-  const machine: IStates = {
-    state: "loading",
-    report: 0,
-    category: 0,
-    product: 0,
-  };
-  useEffect(() => {
-    console.log("in the states context");
-    if (!loading) {
-      console.log(
-        `states:\n
-      report:${data.numOfReports}\n
-      category:${data.numOfCategories}\n
-      product:${data.numOfReports}\n`
-      );
-    } else {
-      console.log("trying to get the states from the server");
-    }
-  }, [loading]);
+
+  // useEffect(() => {
+  //   console.log("in the states context");
+  //   if (!loading && data) {
+  //     console.log(
+  //       `states:\n
+  //     report:${data.numOfReports}\n
+  //     category:${data.numOfCategories}\n
+  //     product:${data.numOfReports}\n`
+  //     );
+  //     states.state = "";
+  //     states.report = data?.numOfReports;
+  //     states.category = data?.numOfCategories;
+  //     states.product = data?.numOfProducts;
+  //   } else if (!loading && !data) {
+  //     console.log("not loading and not data");
+  //   } else {
+  //     console.log("trying to get the states from the server");
+  //   }
+  // }, [loading]);
 
   ///display loading page
   //request amount of report, categories and products
-
-  const states = machine;
-  if (loading) return null;
 
   return (
     <StatesContext.Provider
       value={{
         states,
       }}
-    ></StatesContext.Provider>
+    >
+      {children}
+    </StatesContext.Provider>
   );
 };
 
