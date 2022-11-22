3a3448f81817a329de765c0a946d79e9d273efea
ptrickd <patrickdion8@gmail.com>
2022-11-22 09:33:45 -0700

    Wizard page update

--- pages/wiz.tsx
+++ pages/wiz.tsx
@@ -1,11 +1,11 @@
 //React
-import React, { useContext, Fragment } from "react";
+import React, { useState, useEffect, useContext, Fragment } from "react";
 
 //GraphQL
 import { gql } from "@apollo/client";
 
 //Material UI
-import { makeStyles, Theme, createStyles } from "@material-ui/core";
+import { makeStyles, Theme, createStyles, Typography } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
@@ -19,15 +19,14 @@ import { UserContext } from "../contexts/UserContext";
 import { ReportsContext } from "../contexts/ReportsContext";
 import { CategoriesContext } from "../contexts/CategoriesContext";
 import { ProductsContext } from "../contexts/ProductsContext";
+import { useRouter } from "next/router";
 
-//Queries
-const GET_STATES = gql`
-  query States {
-    numOfReports
-    numOfCategories
-    numOfProducts
-  }
-// `;
+//Define types
+interface IStates {
+  reports: any[] | null;
+  categories: any[] | null;
+  products: any[] | null;
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -44,9 +43,52 @@ const useStyles = makeStyles((theme: Theme) =>
 //Reformating three firstPages in one.
 const Wiz: React.FC = () => {
   const classes = useStyles();
+  const [states, setStates] = useState<IStates>({
+    reports: null,
+    categories: null,
+    products: null,
+  });
   const { loggedIn } = useContext(UserContext);
-  const { numOfReports } = useContext(ReportsContext);
-  const ComponentToDisplay: React.FC = () => <FirstReport />;
+  const { reports } = useContext(ReportsContext);
+  const { categories } = useContext(CategoriesContext);
+  const { products } = useContext(ProductsContext);
+  const router = useRouter();
+
+  //Protecting route
+  useEffect(() => {
+    if (!loggedIn) router.push("/");
+  }, []);
+
+  //Updating states
+  useEffect(() => {
+    setStates({
+      reports: reports || null,
+      categories: categories || null,
+      products: products || null,
+    });
+  }, [reports, categories, products]);
+
+  if (!states.reports || !states.categories || !states.products)
+    return (
+      <Fragment>
+        <CssBaseline />
+        <Container maxWidth="md" className={classes.root}>
+          <Typography>Server Error</Typography>
+        </Container>
+      </Fragment>
+    );
+  const ComponentToDisplay: React.FC = () => {
+    if (states?.reports?.length === 0) return <FirstReport />;
+    else if (states?.categories?.length === 0) return <FirstCategory />;
+    else if (states?.products?.length === 0) return <FirstProduct />;
+    else {
+      return (
+        <Fragment>
+          <Typography>Page Error</Typography>
+        </Fragment>
+      );
+    }
+  };
 
   return (
     <Fragment>
