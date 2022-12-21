c80a0281a47f99cd88387fce596bce1d54976d62
ptrickd <patrickdion8@gmail.com>
2022-12-20 18:21:16 -0700

    Update graphql

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,34 +1,18 @@
-import { merge } from 'lodash'
+import { merge } from "lodash";
 
 //schema.js
-import {
-    typeDef as Product,
-    resolvers as productResolver
-} from "./product";
-import {
-    typeDef as Category,
-    resolvers as categoryResolver
-} from "./category";
-import {
-    typeDef as User,
-    resolvers as userResolver
-} from "./user";
-import {
-    typeDef as Report,
-    resolvers as reportResolver
-} from './report'
-import {
-    makeExecutableSchema
-} from "graphql-tools";
+import { typeDef as Product, resolvers as productResolver } from "./product";
+import { typeDef as Category, resolvers as categoryResolver } from "./category";
+import { typeDef as User, resolvers as userResolver } from "./user";
+import { typeDef as Report, resolvers as reportResolver } from "./report";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 export const schema = makeExecutableSchema({
-    typeDefs: [Product, Category, User, Report],
-    resolvers: merge(
-        productResolver,
-        categoryResolver,
-        reportResolver,
-        userResolver
-    )
-
-})
-
+  typeDefs: [Product, Category, User, Report],
+  resolvers: merge(
+    productResolver,
+    categoryResolver,
+    reportResolver,
+    userResolver
+  ),
+});
