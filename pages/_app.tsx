ff9159d1e2dfce3922490c0ac8ca32dcdbaf4484
ptrickd <patrickdion8@gmail.com>
2023-01-10 17:23:48 -0700

    Npm install roboto font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Themes from "../styles/Themes";
 
+//Roboto Font
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
